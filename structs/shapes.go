0ff28ff07a3baa88ff3721281dd08bd63a16c366
khajornkiat <khajornkiat.us@ols.co.th>
2025-09-07 16:40:17 +0700

    finish structs, now I know test table, interface, and structs

--- structs/shapes.go
+++ structs/shapes.go
@@ -3,14 +3,13 @@ package structs
 import "math"
 
 type Shape interface {
-    Area() float64
+	Area() float64
 }
 
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
-
 
 func (r Rectangle) Perimeter() float64 {
 	return 2 * (r.Height + r.Width)
@@ -21,9 +20,18 @@ func (r Rectangle) Area() float64 {
 }
 
 type Circles struct {
-    Radius float64
+	Radius float64
 }
 
 func (c Circles) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Height * t.Base
 }
