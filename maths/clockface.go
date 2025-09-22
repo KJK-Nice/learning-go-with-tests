a8d6af97ec6e0757d6f1a7e124d79d875a82b5e3
khajornkiat <khajornkiat.us@ols.co.th>
2025-09-22 22:10:42 +0700

    maths, finish the SVGWriter for minute hand

--- maths/clockface.go
+++ maths/clockface.go
@@ -11,12 +11,23 @@ type Point struct {
 	Y float64
 }
 
-func secondInRadians(t time.Time) float64 {
+func secondsInRadians(t time.Time) float64 {
 	return (math.Pi / (30 / float64(t.Second())))
 }
 
 func secondHandPoint(t time.Time) Point {
-	angle := secondInRadians(t)
+	return angleToPoint(secondsInRadians(t))
+}
+
+func minutesInRadians(t time.Time) float64 {
+	return (secondsInRadians(t) / 60) + (math.Pi / (30 / float64(t.Minute())))
+}
+
+func minuteHandPoint(t time.Time) Point {
+	return angleToPoint(minutesInRadians(t))
+}
+
+func angleToPoint(angle float64) Point {
 	x := math.Sin(angle)
 	y := math.Cos(angle)
 
--- maths/clockface_acceptance_test.go
+++ maths/clockface_acceptance_test.go
@@ -3,6 +3,7 @@ package clockface_test
 import (
 	"bytes"
 	"encoding/xml"
+	// "math"
 	"testing"
 	"time"
 
@@ -58,6 +59,32 @@ func TestSVGWriterSecondHand(t *testing.T) {
 
 			if !containsLine(c.line, svg.Line) {
 				t.Errorf("Expected to find the second hand with line %+v, in the SVG lines %v", c.line, svg.Line)
+			}
+		})
+	}
+}
+
+func TestSVGWriterMinuteHand(t *testing.T) {
+	cases := []struct {
+		time time.Time
+		line Line
+	}{
+		{
+			simpleTime(0, 0, 0),
+			Line{150, 150, 150, 70},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			b := bytes.Buffer{}
+			clockface.SVGWriter(&b, c.time)
+
+			svg := SVG{}
+			xml.Unmarshal(b.Bytes(), &svg)
+
+			if !containsLine(c.line, svg.Line) {
+				t.Errorf("Expected to find the minute hand with line %+v, in the SVG lines %v", c.line, svg.Line)
 			}
 		})
 	}
