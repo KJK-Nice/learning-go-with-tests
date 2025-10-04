723549fcb1cdd955903fa9ed214169880c81635a
khajornkiat <khajornkiat.us@ols.co.th>
2025-10-04 15:12:19 +0700

    finish templating

--- blogrenderer/renderer_test.go
+++ blogrenderer/renderer_test.go
@@ -13,7 +13,7 @@ func TestRender(t *testing.T) {
 	var (
 		aPost = blogrenderer.Post{
 			Title:       "hello world",
-			Body:        "This is a post",
+			Body:        "# This is a post",
 			Description: "This is a description",
 			Tags:        []string{"go", "tdd"},
 		}
@@ -35,6 +35,16 @@ func TestRender(t *testing.T) {
 		approvals.VerifyString(t, buf.String())
 	})
 
+	t.Run("it renders an index of posts", func(t *testing.T) {
+		buf := bytes.Buffer{}
+		posts := []blogrenderer.Post{{Title: "Hello World"}, {Title: "Hello World 2"}}
+
+		if err := postRenderer.RenderIndex(&buf, posts); err != nil {
+			t.Fatal(err)
+		}
+
+		approvals.VerifyString(t, buf.String())
+	})
 }
 
 func BenchmarkRender(b *testing.B) {
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/KJK-Nice/learning-go-with-tests
 go 1.24.4
 
 require github.com/approvals/go-approval-tests v0.0.0-20211008131110-0c40b30e0000
+
+require github.com/gomarkdown/markdown v0.0.0-20250810172220-2e2c11897d1a // indirect
