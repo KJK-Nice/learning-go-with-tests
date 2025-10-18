afb73c052e6de902599c819ac1c4df99a4aa7e17
khajornkiat <khajornkiat.us@ols.co.th>
2025-10-18 21:25:45 +0700

    add Record win function

--- http-server/server.go
+++ http-server/server.go
@@ -8,6 +8,7 @@ import (
 
 type PlayerStore interface {
 	GetPlayerScore(name string) int
+	RecordWin(name string)
 }
 
 type PlayerServer struct {
@@ -15,6 +16,17 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w, r)
+	case http.MethodGet:
+		p.showScore(w, r)
+	}
+
+}
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
 
 	score := p.store.GetPlayerScore(player)
@@ -24,7 +36,13 @@ func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprint(w, score)
+}
 
+func (p *PlayerServer) processWin(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	p.store.RecordWin(player)
+	w.WriteHeader(http.StatusAccepted)
+	return
 }
 
 func GetPlayerScore(name string) string {
