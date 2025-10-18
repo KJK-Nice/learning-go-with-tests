5983d50bef824e20d841f34d5fb4438db28e0bdd
khajornkiat <khajornkiat.us@ols.co.th>
2025-10-18 22:21:58 +0700

    add handle safe concurrently to in-memory player store

--- http-server/server_integration_test.go
+++ http-server/server_integration_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"sync"
 	"testing"
 )
 
@@ -11,13 +12,38 @@ func TestRecordingWinsAndRetrievingThem(t *testing.T) {
 	server := PlayerServer{store}
 	player := "Pepper"
 
-	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
-	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
-	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	t.Run("it run record win correctly", func(t *testing.T) {
+		server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+		server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+		server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
 
-	response := httptest.NewRecorder()
-	server.ServeHTTP(response, newGetScoreRequest(player))
-	assertStatus(t, response.Code, http.StatusOK)
+		response := httptest.NewRecorder()
+		server.ServeHTTP(response, newGetScoreRequest(player))
+		assertStatus(t, response.Code, http.StatusOK)
 
-	assertResponseBody(t, response.Body.String(), "3")
+		assertResponseBody(t, response.Body.String(), "3")
+	})
+
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		player := "Tony"
+		wantedCount := 500
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		response := httptest.NewRecorder()
+		server.ServeHTTP(response, newGetScoreRequest(player))
+		assertStatus(t, response.Code, http.StatusOK)
+
+		assertResponseBody(t, response.Body.String(), "500")
+
+	})
 }
