bb0c02bd739cecd42fcfb9e08b5e9e6849642181
khajornkiat <khajornkiat.us@ols.co.th>
2025-10-19 20:04:41 +0700

    add get player score from file system

--- http-server/file_system_store_test.go
+++ http-server/file_system_store_test.go
@@ -26,4 +26,24 @@ func TestFileSystemStore(t *testing.T) {
 		got = store.GetLeague()
 		assertLeague(t, got, want)
 	})
+
+	t.Run("get player score", func(t *testing.T) {
+		database := strings.NewReader(`[
+		{"Name": "Cleo", "Wins": 10},
+		{"Name": "Chris", "Wins": 33}]`)
+
+		store := FileSystemPlayerStore{database}
+
+		got := store.GetPlayerScore("Chris")
+		want := 33
+		assertScoreEqual(t, got, want)
+
+	})
+}
+
+func assertScoreEqual(t testing.TB, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
 }
