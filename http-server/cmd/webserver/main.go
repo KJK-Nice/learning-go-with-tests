72e39134e6621161e74df76d8a9609246c4bd257
khajornkiat <khajornkiat.us@ols.co.th>
2025-10-23 14:44:17 +0700

    refactor project structure for add new cli app

---
+++ http-server/cmd/webserver/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/KJK-Nice/learning-go-with-tests/http-server"
+	"log"
+	"net/http"
+	"os"
+)
+
+const dbFileName = "game.db.json"
+
+func main() {
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		log.Fatalf("problem opening %s %v", dbFileName, err)
+	}
+
+	store, err := poker.NewFileSystemPlayerStore(db)
+	if err != nil {
+		log.Fatalf("problem creating file system player store, %v ", err)
+	}
+
+	server := poker.NewPlayerServer(store)
+
+	if err := http.ListenAndServe(":5001", server); err != nil {
+		log.Fatalf("could no listen on port 5000 %v", err)
+	}
+}
