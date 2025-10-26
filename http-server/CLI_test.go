28544aff666f87ef2042daa570c42e42412ab6c1
khajornkiat <khajornkiat.us@ols.co.th>
2025-10-26 10:55:29 +0700

    implement the blind alerter, schedules all the alerts for 5 players

--- http-server/CLI_test.go
+++ http-server/CLI_test.go
@@ -1,6 +1,7 @@
 package poker_test
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 	"time"
@@ -53,8 +54,41 @@ func TestCLI(t *testing.T) {
 		cli := poker.NewCLI(playerStore, in, blindAlerter)
 		cli.PlayPoker()
 
-		if len(blindAlerter.alerts) != 1 {
-			t.Fatal("expected a blind alert to be scheduled")
+		cases := []struct {
+			expectedScheduleTime time.Duration
+			expectedAmount       int
+		}{
+			{0 * time.Second, 100},
+			{10 * time.Minute, 200},
+			{20 * time.Minute, 300},
+			{30 * time.Minute, 400},
+			{40 * time.Minute, 500},
+			{50 * time.Minute, 600},
+			{60 * time.Minute, 800},
+			{70 * time.Minute, 1000},
+			{80 * time.Minute, 2000},
+			{90 * time.Minute, 4000},
+			{100 * time.Minute, 8000},
+		}
+
+		for i, c := range cases {
+			t.Run(fmt.Sprintf("%d scheduled for %v", c.expectedAmount, c.expectedAmount), func(t *testing.T) {
+				if len(blindAlerter.alerts) <= i {
+					t.Fatalf("alert %d was not schedules %v", i, blindAlerter.alerts)
+				}
+
+				alert := blindAlerter.alerts[i]
+
+				amountGot := alert.amount
+				if amountGot != c.expectedAmount {
+					t.Errorf("got amount %d, want %d", amountGot, c.expectedAmount)
+				}
+
+				gotScheduledTime := alert.scheduledAt
+				if gotScheduledTime != c.expectedScheduleTime {
+					t.Errorf("got schedule time %v, want %v", gotScheduledTime, c.expectedScheduleTime)
+				}
+			})
 		}
 	})
 }
