a93bfea07ce5f0d54c63fab1ae870be2b7b0cde2
khajornkiat <khajornkiat.us@ols.co.th>
2025-10-26 11:09:05 +0700

    refactor cli test suite

--- http-server/CLI_test.go
+++ http-server/CLI_test.go
@@ -9,18 +9,21 @@ import (
 	poker "github.com/KJK-Nice/learning-go-with-tests/http-server"
 )
 
-type SpyBlindAlerter struct {
-	alerts []struct {
-		scheduledAt time.Duration
-		amount      int
-	}
+type scheduledAlert struct {
+	at     time.Duration
+	amount int
 }
 
-func (s *SpyBlindAlerter) ScheduleAlertAt(duration time.Duration, amount int) {
-	s.alerts = append(s.alerts, struct {
-		scheduledAt time.Duration
-		amount      int
-	}{duration, amount})
+func (s scheduledAlert) String() string {
+	return fmt.Sprintf("%d chips at %v", s.amount, s.at)
+}
+
+type SpyBlindAlerter struct {
+	alerts []scheduledAlert
+}
+
+func (s *SpyBlindAlerter) ScheduleAlertAt(at time.Duration, amount int) {
+	s.alerts = append(s.alerts, scheduledAlert{at, amount})
 }
 
 var dummySpyAlerter = &SpyBlindAlerter{}
@@ -54,10 +57,7 @@ func TestCLI(t *testing.T) {
 		cli := poker.NewCLI(playerStore, in, blindAlerter)
 		cli.PlayPoker()
 
-		cases := []struct {
-			expectedScheduleTime time.Duration
-			expectedAmount       int
-		}{
+		cases := []scheduledAlert{
 			{0 * time.Second, 100},
 			{10 * time.Minute, 200},
 			{20 * time.Minute, 300},
@@ -71,24 +71,28 @@ func TestCLI(t *testing.T) {
 			{100 * time.Minute, 8000},
 		}
 
-		for i, c := range cases {
-			t.Run(fmt.Sprintf("%d scheduled for %v", c.expectedAmount, c.expectedAmount), func(t *testing.T) {
+		for i, want := range cases {
+			t.Run(fmt.Sprint(want), func(t *testing.T) {
 				if len(blindAlerter.alerts) <= i {
 					t.Fatalf("alert %d was not schedules %v", i, blindAlerter.alerts)
 				}
 
-				alert := blindAlerter.alerts[i]
+				got := blindAlerter.alerts[i]
+				assertScheduledAlert(t, got, want)
 
-				amountGot := alert.amount
-				if amountGot != c.expectedAmount {
-					t.Errorf("got amount %d, want %d", amountGot, c.expectedAmount)
-				}
-
-				gotScheduledTime := alert.scheduledAt
-				if gotScheduledTime != c.expectedScheduleTime {
-					t.Errorf("got schedule time %v, want %v", gotScheduledTime, c.expectedScheduleTime)
-				}
 			})
 		}
 	})
+}
+
+func assertScheduledAlert(t testing.TB, got, want scheduledAlert) {
+	t.Helper()
+
+	if got.amount != want.amount {
+		t.Errorf("got amount %d, want %d", got.amount, want.amount)
+	}
+
+	if got.at != want.at {
+		t.Errorf("got schedule time %v, want %v", got.at, want.at)
+	}
 }
